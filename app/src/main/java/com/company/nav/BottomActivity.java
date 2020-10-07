1b03e38dd3efcfb6443d9d0700b6694e577bc519
nanonixx <nhid@elpuig.xeill.net>
2020-10-07 19:48:51 +0200

    Initial commit

---
+++ app/src/main/java/com/company/nav/BottomActivity.java
@@ -0,0 +1,14 @@
+package com.company.nav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom);
+    }
+}
